5416170bfe87493f5c30fc874df81078d6c6b379
aidyai <aidyosu553@gmail.com>
2023-02-27 10:24:00 -0800

    jst green

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ import uvicorn
 
 
 
-#if __name__ == "__main__":
-#  uvicorn.run("src.main:app", host="0.0.0.0", port=10000, reload=True)
-
 if __name__ == "__main__":
-  uvicorn.run("src.main:app", port=8000, reload=True)
+  uvicorn.run("src.main:app", host="0.0.0.0", port=10000, reload=True)
+
+#if __name__ == "__main__":
+#  uvicorn.run("src.main:app", port=8000, reload=True)
 
